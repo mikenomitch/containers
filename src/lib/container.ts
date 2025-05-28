88395d49bc153d8f394caff6b115903af69ced3c
Ben Allfree <ben@benallfree.com>
2025-05-27 17:09:29 -0700

    Fix missing <Env> generic

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -157,7 +157,7 @@ class ContainerState {
 /**
  * Main Container class
  */
-export class Container<Env = unknown> extends DurableObject {
+export class Container<Env = unknown> extends DurableObject<Env> {
   // Default port for the container (undefined means no default port)
   defaultPort?: number;
 
